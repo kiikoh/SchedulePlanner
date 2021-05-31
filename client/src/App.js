110e747f55f25e68ea990c543aa78a40992614f9
kiikoh <ikikomonkkey@gmail.com>
2021-05-31 11:27:41 -0400

    added navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Drawer, List, ListItem, ListItemText, Divider, ListItemIcon } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Menu, Inbox, Mail } from "@material-ui/icons";
+import { useState } from "react";
+import TopNav from "./Components/TopNav";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#d32f2f",
+		},
+		secondary: {
+			main: "#e1f5fe",
+		},
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [open, setOpen] = useState(false);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<TopNav open={open} setOpen={setOpen} />
+			<Drawer open={open} onClose={() => setOpen(false)}>
+				<div role="presentation" onClick={() => setOpen(false)} onKeyDown={() => setOpen(false)}>
+					<List>
+						<ListItem button>
+							<ListItemIcon>
+								<Mail />
+							</ListItemIcon>
+							<ListItemText primary={"View Schedules"} />
+						</ListItem>
+					</List>
+				</div>
+			</Drawer>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ client/src/Components/TopNav.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
+
+const TopNav = ({ open, setOpen }) => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+						onClick={() => setOpen(!open)}
+					>
+						<Menu />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						Schedule Planner
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+};
+
+export default TopNav;
