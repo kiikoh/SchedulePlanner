2d484b1042166110484caf88b9d6fb5c2c96391b
kiikoh <ikikomonkey@gmail.com>
2021-06-02 16:35:42 -0400

    progress on FE

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,14 +1,19 @@
-import { TextField } from "@material-ui/core";
-import React from "react";
+import { TextField, Typography } from "@material-ui/core";
+import React, {useState} from "react";
 
 const Login = () => {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<form>
-			<div style={{ marginTop: 25 }}>
-				<TextField required label="Email" variant="outlined" fullWidth />
+			<Typography variant="h4">Login</Typography>
+			<div style={{ marginTop: 10 }}>
+				<TextField required label="Email" variant="outlined" fullWidth onChange={e => setEmail(e.target.value)} value={email}/>
 			</div>
 			<div style={{ marginTop: 10 }}>
-				<TextField required label="Password" variant="outlined" type="password" fullWidth />
+				<TextField required label="Password" variant="outlined" type="password" fullWidth onChange={e => setPassword(e.target.value)} value={password}/>
 			</div>
 		</form>
 	);
--- client/src/Components/Registration.jsx
+++ client/src/Components/Registration.jsx
@@ -1,11 +1,15 @@
-import { TextField } from "@material-ui/core";
+import { TextField, Typography } from "@material-ui/core";
 import React from "react";
 
 const Registration = () => {
 	return (
 		<form>
-			<div style={{ marginTop: 25 }}>
+			<Typography variant="h4">Register</Typography>
+			<div style={{ marginTop: 10 }}>
 				<TextField required label="Email" variant="outlined" fullWidth />
+			</div>
+			<div style={{ marginTop: 10 }}>
+				<TextField required label="Name" variant="outlined" fullWidth />
 			</div>
 			<div style={{ marginTop: 10 }}>
 				<TextField required label="Password" variant="outlined" type="password" fullWidth />
